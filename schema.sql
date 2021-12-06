19961df93642636386b8ec2a599b9b003572edef
Corentin MAS <corentinmas@yahoo.com>
2021-12-06 17:40:38 +0100

    [feature/docker-compose] Tested and functional PGSQL schema.sql

--- schema.sql
+++ schema.sql
@@ -1,10 +1,9 @@
-# Language: PostgreSQL
-
 CREATE TABLE "Users" (
     "id" SERIAL,
     "username" VARCHAR(255) NOT NULL,
     "password" VARCHAR(255) NOT NULL,
     "created_at" TIMESTAMP NOT NULL,
     "updated_at" TIMESTAMP NOT NULL,
-    PRIMARY KEY ("id")
+    PRIMARY KEY ("id"),
+    CONSTRAINT "Users_username_key" UNIQUE ("username")
 );
