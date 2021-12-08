990d6db73f5ed78708e70784853e35d85cf8f882
Corentin MAS <corentinmas@yahoo.com>
2021-12-08 09:48:08 +0100

    [feature/basic-auth] Added basic auth routes and body parser

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,15 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import routes from './entities/routes';
+import bodyParser from 'body-parser';
 
 dotenv.config();
 
 const app = express();
+
+app.use(bodyParser.json());
+
+app.use(routes);
 
 app.listen(3000, () => {
     console.log('listening on port 3000');
